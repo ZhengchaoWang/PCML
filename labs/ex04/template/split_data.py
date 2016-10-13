14ca47ddfc4d14816ebb73944bfc3d9ca6d3240d
ZhengchaoWang <zhengchao.wang@epfl.ae>
2016-10-13 23:33:15 +0800

    Update split_data.py

--- labs/ex04/template/split_data.py
+++ labs/ex04/template/split_data.py
@@ -16,4 +16,33 @@ def split_data(x, y, ratio, seed=1):
     # INSERT YOUR CODE HERE
     # split the data based on the given ratio: TODO
     # ***************************************************
-    raise NotImplementedError
+    
+    def split_data(x, y, ratio, seed=1):
+    """split the dataset based on the split ratio."""
+    # set seed
+    np.random.seed(seed)
+    # ***************************************************
+    # INSERT YOUR CODE HERE
+    # split the data based on the given ratio: TODO
+    # ***************************************************
+    trainDataLen = round(len(y)*ratio)
+    
+    trainDataID = random.sample(range(len(y)), trainDataLen)
+    
+    # USing bool value to obtaint he remainling data for validation data set
+    validDataID = np.array(range(len(y))) + 1
+    validDataID[trainDataID] = 0
+    validDataID = validDataID >0
+    
+    
+    # obtain the trainning data
+    trainDataX = x[trainDataID]
+    trainDataY = y[trainDataID]
+    
+    # obtain the validation data
+    validDataX = x[validDataID]
+    validDataY = y[validDataID]    
+    
+    return trainDataX,trainDataY, validDataX, validDataY
+    
+    #raise NotImplementedError
